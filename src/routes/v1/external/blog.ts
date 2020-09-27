2b46e2f1d6765a68b83211d537c8c01b7a9e3b73
Luke Whrit <lukewhrit@gmail.com>
2020-09-26 23:35:12 -0400

    chore: add license headers to all source files

--- src/routes/v1/external/blog.ts
+++ src/routes/v1/external/blog.ts
@@ -1,5 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 import express from 'express';
-import BlogController from '../../../controllers/blog.controller';
 
 // @todo Return all of the rows in from the blog.
 
